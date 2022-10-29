fa9ed313cd4e61fda8099e2bae0851c241e2c02b
Cyrus Barkhurst <63369999+Cyrus587@users.noreply.github.com>
2022-10-28 20:15:06 -0500

    Add files via upload

---
+++ chatbotthree.java
@@ -0,0 +1,73 @@
+import javax.swing.* ;
+
+public class chatbotthree extends JFrame {
+     static JTextArea Chatarea = new JTextArea();
+     static JTextField Chatbox = new JTextField();
+
+    
+    public static void main(final String[] args){
+        final JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setSize(600,600);
+        frame.setTitle("CHATBOT");
+        frame.add(Chatarea);
+        frame.add(Chatbox);
+        //For ttext area
+        Chatarea.setSize(500,400);
+        Chatarea.setLocation(2,2);
+        //For textfeild
+        Chatbox.setSize(540,30);
+        Chatbox.setLocation(2,500);
+
+        Chatbox.addActionListener(new ActionListener());
+            @Override
+    ````````public void actionPerformed(ActionEvent arg0){
+                String gtext = Chatbox.getText();
+                Chatarea.append("You ----> "+ gtext + "\n");
+                Chatbox.setText("");
+
+                
+                
+                }
+            }
+        
+    
+
+        
+        
+    
+
+
+
+ 
+       // Field.addActionListener(New ActionListener){
+           
+           // @Override
+            /*public void actionPerformed(ActionEvent e){
+                String msg = Field.getText();
+                txt.append("You = " + msg + "\n");
+                Field.setText("");
+                if(msg.contains("hi"))*/
+  
+                
+                
+            
+    
+    
+
+                
+            
+        
+
+        
+
+
+
+            
+            
+    
+    
+
